b711f3c1f1cfa943c24537aa9d7b371da6b8e0aa
Drew Wyatt <drew.wyatt@classdojo.com>
2023-02-05 10:38:05 -0500

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,4 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+const file = await Deno.readTextFile("./data/adjectives.txt");
+const adjectives = file.split("\n").filter(Boolean);
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+console.log("adj", file);
